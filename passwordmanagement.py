2b47d721a954f31eedf8b727f6b4b8abd98bedae
Adam Mawani <142749950+AdamMawani@users.noreply.github.com>
2024-04-10 20:56:36 -0400

    Cleaned up code

--- passwordmanagement.py
+++ passwordmanagement.py
@@ -5,27 +5,21 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-# Dictionary to store user credentials
 user_credentials = {}
 
 def check_password_strength(password):
-    # Check if password length is at least 8 characters
     if len(password) < 8:
         return False
     
-    # Check if password contains at least one uppercase letter
     if not re.search(r'[A-Z]', password):
         return False
     
-    # Check if password contains at least one lowercase letter
     if not re.search(r'[a-z]', password):
         return False
     
-    # Check if password contains at least one digit
     if not re.search(r'\d', password):
         return False
     
-    # Check if password contains at least one special character
     if not re.search(r'[!@#$%^&*()_+{}|:"<>?`\-=[\];\',./]', password):
         return False
     
